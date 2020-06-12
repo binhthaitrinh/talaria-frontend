5f3c1780da578c4ba78e2c916021c93525916b0e
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2020-06-12 12:36:54 -0700

    fix _document

--- pages/_document.js
+++ pages/_document.js
@@ -2,12 +2,20 @@ import Document, { Html, Main, NextScript, Head } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static async getInitialProps({ renderPage }) {
+  static getInitialProps({ renderPage }) {
+    // Step 1: Create an instance of ServerStyleSheet
     const sheet = new ServerStyleSheet();
+
+    // Step 2: Retrieve styles from components in the page
     const page = renderPage((App) => (props) =>
       sheet.collectStyles(<App {...props} />)
     );
-    return { ...page, styles: sheet.getStyleElement() };
+
+    // Step 3: Extract the styles as <style> tags
+    const styleTags = sheet.getStyleElement();
+
+    // Step 4: Pass styleTags as a prop
+    return { ...page, styleTags };
   }
   // static async getStaticProps(ctx) {
   //   const sheet = new ServerStyleSheet();
