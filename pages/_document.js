50c594b181bd56f3e2e82b18c4a073f127152a00
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2020-06-12 12:31:59 -0700

    fix _document

--- pages/_document.js
+++ pages/_document.js
@@ -2,31 +2,38 @@ import Document, { Html, Main, NextScript, Head } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static async getStaticProps(ctx) {
+  static async getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        });
-
-      const initialProps = await Document.getStaticProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
-    }
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    return { ...page, styles: sheet.getStyleElement() };
   }
+  // static async getStaticProps(ctx) {
+  //   const sheet = new ServerStyleSheet();
+  //   const originalRenderPage = ctx.renderPage;
+
+  //   try {
+  //     ctx.renderPage = () =>
+  //       originalRenderPage({
+  //         enhanceApp: (App) => (props) =>
+  //           sheet.collectStyles(<App {...props} />),
+  //       });
+
+  //     const initialProps = await Document.getStaticProps(ctx);
+  //     return {
+  //       ...initialProps,
+  //       styles: (
+  //         <>
+  //           {initialProps.styles}
+  //           {sheet.getStyleElement()}
+  //         </>
+  //       ),
+  //     };
+  //   } finally {
+  //     sheet.seal();
+  //   }
+  // }
 
   render() {
     return (
