87e9e69fbe43bf099cd001bbe1c8910e2c192132
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2020-06-19 13:21:02 -0700

    distinguish between prod and dev env var

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
+const prod = process.env.NODE_ENV === 'production';
+
 module.exports = {
   env: {
-    BASE_URL: 'http://www.localhost:4444/api/v1',
+    BASE_URL: prod
+      ? 'https://hidden-gorge-76682.herokuapp.com/api/v1'
+      : 'http://localhost:4444/api/v1',
   },
 };
