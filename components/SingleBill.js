433b536d6549543148a66897e2b64671953791a4
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2020-06-19 13:50:50 -0700

    add bill detail page

--- components/SingleBill.js
+++ components/SingleBill.js
@@ -81,14 +81,104 @@ const SingleItem = (props) => {
           <DetailItemInfo>{item.status}</DetailItemInfo>
         </DetailItem>
         <DetailItem>
+          <DetailItemTitle>Tổng bill in VND</DetailItemTitle>
+          <DetailItemInfo>
+            {new Intl.NumberFormat('de-DE', {
+              style: 'currency',
+              currency: 'VND',
+            }).format(item.moneyChargeCustomerVND['$numberDecimal'])}
+          </DetailItemInfo>
+        </DetailItem>
+        <DetailItem>
           <DetailItemTitle>Tiền đã nhận</DetailItemTitle>
           <DetailItemInfo>
-            {' '}
-            {new Date(item.date).toLocaleString('en-us', {
-              month: 'long',
-              year: 'numeric',
-              day: 'numeric',
-            })}
+            <StickerBtn type="success">
+              {new Intl.NumberFormat('de-DE', {
+                style: 'currency',
+                currency: 'VND',
+              }).format(item.moneyReceived['$numberDecimal'])}
+            </StickerBtn>
+          </DetailItemInfo>
+        </DetailItem>
+        <DetailItem>
+          <DetailItemTitle>Số tiền còn thiếu</DetailItemTitle>
+          <DetailItemInfo>
+            <StickerBtn type="danger">
+              {new Intl.NumberFormat('de-DE', {
+                style: 'currency',
+                currency: 'VND',
+              }).format(item.remaining['$numberDecimal'])}
+            </StickerBtn>
+          </DetailItemInfo>
+        </DetailItem>
+        <DetailItem>
+          <DetailItemTitle>Tỉ giá VND/USD</DetailItemTitle>
+          <DetailItemInfo>
+            {new Intl.NumberFormat('de-DE', {
+              style: 'currency',
+              currency: 'VND',
+            }).format(item.vndUsdRate['$numberDecimal'])}
+          </DetailItemInfo>
+        </DetailItem>
+        <DetailItem>
+          <DetailItemTitle>Cân nặng ước tính</DetailItemTitle>
+          <DetailItemInfo>
+            {item.estimatedWeight['$numberDecimal']} lbs
+          </DetailItemInfo>
+        </DetailItem>
+        <DetailItem>
+          <DetailItemTitle>Phí ship về VN in USD</DetailItemTitle>
+          <DetailItemInfo>
+            {new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: 'USD',
+            }).format(item.shippingFeeToVnInUSD['$numberDecimal'])}
+          </DetailItemInfo>
+        </DetailItem>
+        <DetailItem>
+          <DetailItemTitle>Tổng số tiền in USD</DetailItemTitle>
+          <DetailItemInfo>
+            {new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: 'USD',
+            }).format(item.totalBillInUsd['$numberDecimal'])}
+          </DetailItemInfo>
+        </DetailItem>
+        <DetailItem>
+          <DetailItemTitle>Tổng số tiền in USD (sau giảm giá)</DetailItemTitle>
+          <DetailItemInfo>
+            {new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: 'USD',
+            }).format(item.moneyChargeCustomerUSD['$numberDecimal'])}
+          </DetailItemInfo>
+        </DetailItem>
+        <DetailItem>
+          <DetailItemTitle>Khách hàng</DetailItemTitle>
+          <DetailItemInfo>
+            <Link href={`/customers/${item.customer._id}`} passHref>
+              <LinkPrimary> {item.customer.customerName}</LinkPrimary>
+            </Link>
+          </DetailItemInfo>
+        </DetailItem>
+        <DetailItem>
+          <DetailItemTitle></DetailItemTitle>
+          <DetailItemInfo></DetailItemInfo>
+        </DetailItem>
+        <DetailItem>
+          <DetailItemTitle>Items</DetailItemTitle>
+          <DetailItemInfo>
+            <ul>
+              {item.items.map((doc) => (
+                <li>
+                  <Link href={`/items/${doc._id}`} passHref>
+                    <a>
+                      {doc.quantity} x {doc.name}
+                    </a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
           </DetailItemInfo>
         </DetailItem>
       </DetailList>
